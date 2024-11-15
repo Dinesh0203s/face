4690296d93e01c334fe003a35bbe1931c901d60f
Dinesh0203s <dineshsenathipathi@gmail.com>
2024-11-15 12:07:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,4 +42,5 @@ def serve_image(filename):
     return send_from_directory('static/output_photos', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
